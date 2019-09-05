e9d997bb0c6031178ab0002cdac2513669c15a13
Aaron Rennow <arennow@outlook.com>
2019-09-05 06:07:28 -0400

    Initial Commit

---
+++ arsh/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  arsh
+//
+//  Created by Aaron Rennow on 9/5/19.
+//  Copyright © 2019 Lithium Cube. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
